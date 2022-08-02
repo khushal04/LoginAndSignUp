cc5ec62e1ed877ce9ef95b8f189c02b99dbae06c
khushal04 <chandakk008@gmail.com>
2022-08-02 20:50:18 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/chatsecure/RegisterActivity.java
@@ -0,0 +1,9 @@
+package com.example.chatsecure;
+
+import android.app.Activity;
+import android.view.View;
+
+public class RegisterActivity extends Activity {
+    public void onLoginClick(View view) {
+    }
+}
